f8632f89a59a8ae7ac42bb41cb2a1234a9d1fd7e
Aboelkasem Elsayed <151479051+Aboelkasem1@users.noreply.github.com>
2024-09-10 01:15:53 +0300

    Update G - Tanya and Stairways.cpp

--- STLs 1/G - Tanya and Stairways.cpp
+++ STLs 1/G - Tanya and Stairways.cpp
@@ -1,3 +1,7 @@
+////////////////
+// solution 1 //
+////////////////
+
 // #include <bits/stdc++.h>
 // using namespace std;
 //
@@ -54,7 +58,10 @@
 //
 // }
 //
-// //2
+
+////////////////
+// solution 2 //
+////////////////
 
 #include <bits/stdc++.h>
 using namespace std;
